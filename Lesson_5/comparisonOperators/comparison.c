fa0ef39ab71143df387ba5b1e506e0bc01146aa9
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-14 15:34:29 +0300

    C

---
+++ Lesson_5/comparisonOperators/comparison.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+                    // Comparison Operators
+
+int main()
+{
+
+    /*
+        Comparison Operators
+
+        ==  !=  >   <   >=  <=
+    */
+
+    int x = 15, y = 20;
+
+    printf("%d", x == y);   // == "eşit mi?" operatörüdür. eğer eşitse true yani 1 değerini döndürür değilse false (0) döndürür.
+                            // != eşit mi operatörünün tersidir.
+    
+    return 0;
+}
---
+++ Lesson_5/logicalOperators/logical.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+                    // Logical Operators
+
+int main()
+{
+
+    /*
+        Logical Operators
+
+        &&      ||      !
+    */
+    /*
+        &&  ==> iki değerde true olursa true çıktısı verir
+        
+        ||  ==> iki önermeden en az biri true olursa true çıktısı verir.
+        
+         !  ==> önermenin değerini tersine çevirir.
+    */
+ 
+
+    int x = 15, y = 20;
+
+    printf("%d", x < y && x>0);   
+    
+    return 0;
+}
