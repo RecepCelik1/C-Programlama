a97c03861d0893256664399ffe570f1a682837d2
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-20 10:22:47 +0300

    C

---
+++ Lesson_8/math.h/libraryMath.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    
+    /*  ceil  floor  sqrt  pow  abs
+    
+    * ceil (tavan demek) = 4.8 = 5.0 
+    
+    * floor (taban) = 4.8 = 4.0
+    
+    * sqrt (kare) = karekök alma
+    
+    * pow (üs) = üs alma
+    
+    * abs (absolute = tam, mutlak) = mutlak değere alma
+    
+    */
+    
+    printf("%.3f \n",ceil(4.5));
+    
+    printf("%.3f \n",floor(4.5));
+    
+    printf("%.3f \n",sqrt(4.5));
+    
+    printf("%.3f \n",pow(4.5,2));
+
+    printf("%.3f \n",abs(6));
+    
+    return 0;
+}
