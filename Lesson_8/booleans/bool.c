908a11e2dd9e204d745aae2e50c2598b6f9f6827
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-20 09:43:30 +0300

    C

---
+++ Lesson_8/booleans/bool.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>  // bu kütüphane olmadan bool data türü çalışmaz
+
+                    //boolen data türü 
+                    // true(1)     or     false(0)
+
+int main() {
+    bool x = false;
+    printf("%d",x);
+
+    return 0;
+}
