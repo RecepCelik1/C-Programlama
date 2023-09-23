0868c2c9f9459d75f321e0b93bbe77e893132a85
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-23 09:00:23 +0300

    C

---
+++ Lesson_9/2dArray/2dArrays.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+                    //Çok boyutlu sayı dizileri
+
+int main() {
+
+            /*      Çok boyutlu sayı dizileri
+
+                # Matris oluşturmak için kullanılır.
+
+                # Kodların hafızada tutulan kısmı doğrusal olmasına rağmen çok boyutlu sayı dizileri
+                doğrusal değildir. O halde çok boyutlu dizileri doğrusal hale getirmek için dizinin
+                dizisi şeklinde yazmamız gerekir.
+
+                # Dizinin içinde dizi : Bir dizinin içindeki tüm elemanların dizi olması durumu.
+            
+            */
+
+
+
+    return 0;
+}
