83ca73d82251037ba4bf7101c275fce9f7a619a5
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-23 11:28:52 +0300

    C

--- Lesson_9/sumOfMatrixes/sumOfMatrixes.c
+++ Lesson_9/sumOfMatrixes/sumOfMatrixes.c
@@ -6,17 +6,17 @@ int main() {
     //defining to variables statement
 
     int i = 0, m = 0;
-    int matrix1[3][3] = { {1,2,3} , {4,5,6} , {7,8,9} };
+    int matrix1[3][4] = { {1,2,3,4} , {5,6,7,8} , {9,10,11,12} };
     
-    int matrix2[3][3] = { {9,8,7} , {6,5,4} , {3,2,1} };
+    int matrix2[3][4] = { {12,11,10,9} , {8,7,6,5} , {4,3,2,1} };
     
-    int matrix3[3][3];
+    int matrix3[3][4];
     
     
     
     while(i<3){     //for sum of matrixes
         
-        while(m<3){
+        while(m<4){
         matrix3[i][m] = matrix1[i][m] + matrix2[i][m];
         m++;
         }
@@ -29,7 +29,7 @@ int main() {
     m=0;
     while(i<3){     //for print to matrix3 
        
-       while(m<3){
+       while(m<4){
         printf("%d  ",matrix3[i][m]);
         m++;
        }
