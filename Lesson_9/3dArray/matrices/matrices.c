c94a6800c46e4aca7ffebab062c0a21460facd6b
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-24 20:15:15 +0300

    C

--- Lesson_9/3dArray/matrices/matrices.c
+++ Lesson_9/3dArray/matrices/matrices.c
@@ -6,16 +6,29 @@ int main() {
     int matrices[2][3][2];
     int i = 0, j = 0, k = 0;
     
-    while(i<2){
+    for(i = 0; i<2; i++){
         
         for(j = 0; j<3; j++){
-
             for(k = 0; k<2; k++){
-                printf("Please enter a number [%d] [%d] [%d]:   ",(2-i) , (3-j) , (2-k));
+                printf("Please enter a number [%d] [%d] [%d] : ",(i+1) , (j+1) , (k+1));
                 scanf("%d",&matrices[i][j][k]);
             }
-        
         }
+    }
+    printf("\n\n");
+    for(i = 0; i < 2; i++){
+
+        for(j = 0; j < 3; j++){
+            for(k = 0; k < 2; k++){
+
+                printf("%d  ",matrices[i][j][k]);
+
+            }
+            printf("\n");
+        }
+
+    printf("\n\n\n");
+
     }
 
     return 0;
