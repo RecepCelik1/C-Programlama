227609b48222e6f82c3257ceafc32c982e6d40fe
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-25 13:25:34 +0300

    C

---
+++ Lesson_9/strings/strings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+                        //strings
+
+int main() {
+    int i = 0;
+    char string[3] = {'Y','T','U'};
+
+    for(i=0; i<3; i++){
+
+    printf("%c",string[i]);
+
+    }
+    return 0;
+}
