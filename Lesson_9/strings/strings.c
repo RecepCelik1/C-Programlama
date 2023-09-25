46cd7375f033393ec9a6859e489032cafd1aef2b
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-25 14:11:31 +0300

    C

--- Lesson_9/strings/strings.c
+++ Lesson_9/strings/strings.c
@@ -5,12 +5,35 @@
 
 int main() {
     int i = 0;
-    char string[3] = {'Y','T','U'};
+    
+    char string[] = {'Y','T','U','\0'}; //  \0  ifadesi C programlama da karakterlerin bittiğini ifade eder. 
+
+    char string1[] = "YTU"; 
+
+    /*
+       Yukarıda aynı metin iki farklı şekilde yazıldı derleyici de fakat her ikiside hafızada aynı şekilde depolanır.
+        --------            ---------           ---------           --------
+        |   Y   |          |    T    |         |    U    |         |   \0   |
+        --------            ---------           ---------           --------
+        string[0]           string[1]           string[2]           string[3]
+
+    */
 
     for(i=0; i<3; i++){
 
     printf("%c",string[i]);
 
     }
+
+    printf("\n%s\n\n",string1);
+
+    // karakter dizilerine ulaşmak için %s operatörü kullanılır. (string den geliyor.)
+
+    char stringInput[20]; // eğer değişkeni tanımlayıp yukarıdaki gibi değer ataması yapmadıysak karakter kaç karakter
+                          // tanımlanabileceğini önceden belirtmemiz gerekir.
+
+    printf("Bir string giriniz lütfen : "); scanf("%s",&stringInput);
+    printf("Girdiğiniz string = %s",stringInput);
+
     return 0;
 }
