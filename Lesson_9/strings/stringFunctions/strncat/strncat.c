581c46cfe7dbdcd22dace6ffa5c886f9ab477909
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-26 20:27:53 +0300

    C

---
+++ Lesson_9/strings/stringFunctions/strncat/strncat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+                            //strncat == ilk dizinin sonuna ikinci dizideki ilk n kadar karakteri atama anlamına gelir.
+
+int main(){
+
+    char hello[100] = {};
+    char helloThere[30] = "My favorite food is steak";
+
+    strcat(hello, "My name is obi wan kenobi "); 
+
+    strncat(hello, helloThere, 11);
+
+    printf("%s",hello);
+
+    return 0;
+}
