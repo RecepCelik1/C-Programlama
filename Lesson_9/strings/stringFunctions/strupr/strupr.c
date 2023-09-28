e69acc97add637f5e9656969d93f1814f0a3c5a9
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-28 23:16:24 +0300

    C

--- Lesson_9/strings/stringFunctions/strupr/strupr.c
+++ Lesson_9/strings/stringFunctions/strupr/strupr.c
@@ -8,7 +8,10 @@ int main(){
     
     char strings[30];
 
-    printf("Lütfen bir text giriniz\n");
+    printf("Lütfen bir text giriniz"); scanf("%s",&strings);
+    printf("Girdiğiniz ifade = %s\n",strings); 
+    printf("Girdiğiniz ifadenin büyük harflere dönüştürülmüş hali ise = %s",strupr(strings));
+
 
     return 0;
 }
