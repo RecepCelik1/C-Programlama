99c869222e84004c8ac73860ff1e0309a94f3e16
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-28 23:23:09 +0300

    C

--- Lesson_9/strings/stringFunctions/strstr/strstr.c
+++ Lesson_9/strings/stringFunctions/strstr/strstr.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-                            //strstr()  
+                            //strstr()  bir string içerisinde belirli karakterleri arar.
 
 int main(){
 
-
+    char string[30] = "C dilini öğreniyoruz";
+    char *sub;
+    sub=strstr(string,"dil");
+    printf("\nArama sonrası yeni string: %s",sub);
 
 
     return 0;
