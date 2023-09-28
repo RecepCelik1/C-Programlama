df9900ae8b15e158418cc4127d4c2af388ea8e80
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-28 23:08:55 +0300

    C

---
+++ Lesson_9/strings/stringFunctions/strupr/strupr.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+                                    //strlwr fonksiyonunun tam tersi girilen bir string dizisindeki her karakteri büyük harfe çevirir.
+
+int main(){
+    
+    char strings[30];
+
+    printf("Lütfen bir text giriniz\n");
+
+    return 0;
+}
