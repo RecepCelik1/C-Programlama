73cd2d26a1dfd9016ddf41ce922b3c41b0f1b995
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-09-28 23:33:59 +0300

    C

---
+++ Lesson_10/functions/functions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+                //fonksiyonlar belirlediğimiz işlemleri yapabilmemize olanak sağlayan kod bloklarıdır.
+
+int main(){
+
+
+
+    return 0;
+}
