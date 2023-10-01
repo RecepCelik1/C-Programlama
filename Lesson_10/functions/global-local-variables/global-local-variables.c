f6b5ff6e988a95b7af981c413ab40b10f9b15c77
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-10-01 21:08:00 +0300

    C

--- Lesson_10/functions/global-local-variables/global-local-variables.c
+++ Lesson_10/functions/global-local-variables/global-local-variables.c
@@ -5,14 +5,28 @@
 
     int num;   //==> işte bu global değişkendir
 
+void ObiWanKenobi(){
+    int num1 = 3;
+    printf("%d",num1);
+}
+
 int main(){
 
     int num;   //==> işte bu local değişkendir bi önceki repolarda bahsettiğim minecraft biyom mevzusu
 
     {   
+
         //  fonksiyonlar içerisinde de local alanlar oluşturabiliyoruz aynen bu şekilde görüldüğü gibi
 
             int num;
+            printf("%d",num);  //==> bu print kendisine en yakın olan değişkeni, en locali yazdıracaktır
+
+    }
+
+
+    {
+
+        
 
     }
 
